47f519359f928e2ebe42a709ff2dd728d3d30d17
MEHDI MTIR <mehdi.mtir@gmail.com>
2024-06-03 20:07:57 +0100

    Afficher la liste des books

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
 import './App.css';
+import { useState } from 'react';
+import BooksList from './books/BooksList';
 
 function App() {
+  
+  const [books, setBooks] = useState(
+    [
+      {id : 1, title : "Slight Edge", author : "Jeff Olsen"},
+      {id : 2, title : "Atmic Habits", author : "James Clear"},
+      {id : 3, title : "Power of habits", author : "Charles Duhigg"}
+    ]
+  );
+
   return (
     <div className='container'>
       <h1>Application de gestion de livres :</h1>
+
+      <BooksList books={books} />
     </div>
   );
 }
---
+++ src/books/BooksList.jsx
@@ -0,0 +1,28 @@
+function BooksList(props){
+    return <table className="table">
+    <thead>
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">Titre</th>
+        <th scope="col">Auteur</th>
+        <th scope="col">Editer</th>
+        <th scope="col">Supprimer</th>
+      </tr>
+    </thead>
+    <tbody>
+        {
+            props.books.map(
+                book=> <tr>
+                <th scope="row">{book.id}</th>
+                <td>{book.title}</td>
+                <td>{book.author}</td>
+                <td><button className="btn btn-primary">Editer</button></td>
+                <td><button className="btn btn-danger">Supprimer</button></td>
+              </tr>
+            )
+        }
+    </tbody>
+    </table>
+}
+
+export default BooksList;
